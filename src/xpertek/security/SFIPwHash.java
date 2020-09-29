bc2e33f87a0e434fca207a6682e5fb0599f34b94
firozba <70315649+firozba@users.noreply.github.com>
2020-09-29 16:57:16 +0200

    Initial Load
    Initial Loading of project

---
+++ src/xpertek/security/SFIPwHash.java
@@ -0,0 +1,50 @@
+package xpertek.security;
+
+import java.security.MessageDigest;
+import java.util.Base64;
+
+public class SFIPwHash {
+
+	private static String pw;
+	private static String hash;
+	
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		try {
+			if(args.length>0)
+				pw=(String)args[0];
+			else
+				pw="password";
+			hash = getHash(pw, "SHA");
+			System.out.println("Hash of \""+pw +"\" is "+hash);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String getHash(String data, String algorithm) throws SecurityException {
+		try
+		{
+			if (data == null)
+				return null;
+			
+			if (data.trim().length() == 0)
+				return "";
+			
+			MessageDigest md = MessageDigest.getInstance(algorithm);
+			md.reset();
+			
+			byte[] bin = md.digest(data.getBytes());
+
+			return Base64.getEncoder().encodeToString(bin);			
+
+		}
+		catch (Exception e)
+		{
+			throw new SecurityException(e.getMessage());
+		}
+	}	
+}
